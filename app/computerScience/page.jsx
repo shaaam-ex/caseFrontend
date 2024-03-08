c2ec70677a52ea0edb14b0826e9cabf97bc46842
NOTAHTI123 <ahtishamahmed1234@gmail.com>
2024-03-08 21:46:16 +0500

    First

--- app/computerScience/page.jsx
+++ app/computerScience/page.jsx
@@ -6,7 +6,7 @@ export default function ComputerScience() {
         <>
             <div className="main-container-computer-science">
                 
-                {/* <head> */}
+                {/* <head>
                     <title>Department of Computer Science - Sir Syed CASE Institute of Technology </title>
                     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
                     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
@@ -15,7 +15,7 @@ export default function ComputerScience() {
                     <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
                     <meta name="msapplication-TileColor" content="#da532c" />
                     <meta name="theme-color" content="#ffffff"></meta>
-                {/* </head> */}
+                </head> */}
 
                 <div className="first-banner-container-computer-science">
                     <div className="content-first-banner-container-computer-science">
@@ -35,6 +35,39 @@ export default function ComputerScience() {
                             <div className="click-here-button-right-div-content-first">
                                 <Link className='standard-rectangle-button' href={'#'}>Click Here</Link>
                             </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="second-div-container-computer-science">
+                    <div className="current-directory-div-second-div-computer-science">
+                        <p><a href="/landingPage">Home</a> <span style={{color: 'rgba(0, 0, 0, 0.6)'}}>/</span> Department of Computer Science</p>
+                    </div>
+
+                    <div className="mission-statement-accreditation-div-second-div-container-computer-science">
+                        <div className="mission-statement-div-cs">
+                            <h1>Mission Statement</h1>
+                                <p>
+                                    Prepare future Computer Scientists to achieve excellence in the core 
+                                    competencies of computer science that enable them to effectively develop 
+                                    software solutions for challenges faced in industry and research with the 
+                                    highest level of integrity.
+                                </p>
+                        </div>
+
+                        <div className="accreditation-div-cs">
+                            <h2>Accreditation of BSCS from HEC</h2>
+                            <p>
+                                Accreditation from National Computing Education Accreditation Council (NCEAC).
+
+                                <ol>
+                                    <li>Category/Rating X for two years (2011‐2012)</li>
+                                    <li>Category/Rating W for three years (2013‐2015)</li>
+                                </ol>
+
+                                Zero Visit Accreditation from HEC for BS Artificial Intelligence and Software 
+                                Engineering in Progress.
+                            </p>
                         </div>
                     </div>
                 </div>
