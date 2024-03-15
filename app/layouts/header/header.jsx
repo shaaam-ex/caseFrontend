b0a7f6baa4c7c3d16969a63eaf23c0109703c667
NOTAHTI123 <ahtishamahmed1234@gmail.com>
2024-03-15 19:44:06 +0500

    Headerr Update

--- app/layouts/header/header.jsx
+++ app/layouts/header/header.jsx
@@ -1,9 +1,12 @@
+'use client'
+
 import Link from 'next/link';
 import './header.css';
 import { MdMenu, MdArrowDropDown } from "react-icons/md";
-
+import { useEffect } from 'react';
 
 export default function Header() {
+    
     return (
         <>
             <div className="main-container-header">
@@ -13,11 +16,43 @@ export default function Header() {
 
                 <div className="middle-div-container-header">
                     <ul className='clickable-hoverable-links-header'>
-                        <li><Link href='/landingPage'>Home</Link></li>
-                        <li><Link href='#'>Admissions</Link><MdArrowDropDown className='drop-down-icon' /></li> {/* DropDown */}
-                        <li><Link href='#'>Departments</Link><MdArrowDropDown className='drop-down-icon' /></li> {/* DropDown */}
-                        <li><Link href='#'>Portal</Link><MdArrowDropDown className='drop-down-icon' /></li> {/* DropDown */}
-                        <li><Link href='#'>About</Link></li>
+                        <li>
+                            <Link href='/landingPage'>Home</Link>
+                        </li>
+
+                        <li className='absolute-child-drop-down-container'>
+                            <Link href='#'>Admissions</Link><MdArrowDropDown className='drop-down-icon' />
+
+                            <ul className='container-available-options-drop-down-menu'>
+                                <li className='clickable-available-link-drop-down-menu'><Link className='clickable-link-inner-drop-down' href='#'>Offered Programs</Link></li>
+                                <li className='clickable-available-link-drop-down-menu'><Link className='clickable-link-inner-drop-down' href='#'>How to Apply</Link></li>
+                                <li className='clickable-available-link-drop-down-menu'><Link className='clickable-link-inner-drop-down' href='#'>Eligibility Criteria</Link></li>
+                                <li className='clickable-available-link-drop-down-menu'><Link className='clickable-link-inner-drop-down' href='#'>Scholarships</Link></li>
+                                <li className='clickable-available-link-drop-down-menu'><Link className='clickable-link-inner-drop-down' href='#'>Fee Structure</Link></li>
+                            </ul>
+                        </li> {/* DropDown */}
+
+                        <li className='absolute-child-drop-down-container'>
+                            <Link href='#'>Departments</Link><MdArrowDropDown className='drop-down-icon' />
+
+                            <ul className='container-available-options-drop-down-menu'>
+                                <li className='clickable-available-link-drop-down-menu'><Link className='clickable-link-inner-drop-down' href='#'>Computer Science</Link></li>
+                                <li className='clickable-available-link-drop-down-menu'><Link className='clickable-link-inner-drop-down' href='#'>Electrical Engineering</Link></li>
+                            </ul>
+                        </li> {/* DropDown */}
+
+                        <li className='absolute-child-drop-down-container'>
+                            <Link href='#'>Portal</Link><MdArrowDropDown className='drop-down-icon' />
+
+                            <ul className='container-available-options-drop-down-menu'>
+                                <li className='clickable-available-link-drop-down-menu'><Link className='clickable-link-inner-drop-down' href='#'>Faculty Portal</Link></li>
+                                <li className='clickable-available-link-drop-down-menu'><Link className='clickable-link-inner-drop-down' href='#'>Student Portal</Link></li>
+                            </ul>
+                        </li> {/* DropDown */}
+
+                        <li>
+                            <Link href='#'>About</Link>
+                        </li>
                     </ul>
                 </div>
 
