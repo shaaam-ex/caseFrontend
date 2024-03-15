91faefce80863b3479f79c8d7685cefacfb5484b
NOTAHTI123 <ahtishamahmed1234@gmail.com>
2024-03-15 17:58:28 +0500

    First

--- app/layouts/header/header.jsx
+++ app/layouts/header/header.jsx
@@ -1,5 +1,6 @@
+import Link from 'next/link';
 import './header.css';
-import { MdMenu } from "react-icons/md";
+import { MdMenu, MdArrowDropDown } from "react-icons/md";
 
 
 export default function Header() {
@@ -10,8 +11,19 @@ export default function Header() {
                     <img src="https://www.case.edu.pk/wp-content/uploads/2023/05/CASE-Logo-1.png" alt="" />
                 </div>
 
+                <div className="middle-div-container-header">
+                    <ul className='clickable-hoverable-links-header'>
+                        <li><Link href='/landingPage'>Home</Link></li>
+                        <li><Link href='#'>Admissions</Link><MdArrowDropDown className='drop-down-icon' /></li> {/* DropDown */}
+                        <li><Link href='#'>Departments</Link><MdArrowDropDown className='drop-down-icon' /></li> {/* DropDown */}
+                        <li><Link href='#'>Portal</Link><MdArrowDropDown className='drop-down-icon' /></li> {/* DropDown */}
+                        <li><Link href='#'>About</Link></li>
+                    </ul>
+                </div>
+
                 <div className="right-div-container-header">
-                    <MdMenu className='hamburger-menu-button-header' />
+                    {/* <MdMenu className='hamburger-menu-button-header' /> */}
+                    <img src="https://res.cloudinary.com/dm1hjjfsz/image/upload/v1710505935/case/e0gkiton8cke4sukhjbg.png" alt="" />
                 </div>
             </div>
         </>
