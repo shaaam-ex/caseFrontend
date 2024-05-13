42b7352933021b971a1e5c765772b3cb62b5ea7e
NOTAHTI123 <ahtishamahmed1234@gmail.com>
2024-05-13 22:16:46 +0500

    Update 13/05/2024 10:13pm
    Optimized MainCS Page

--- app/layouts/semesterTable/Table.jsx
+++ app/layouts/semesterTable/Table.jsx
@@ -10,7 +10,7 @@ const Table = (props) => {
                     <h3 className='h3-code-column-inner-heading-main-semester-table'>Code</h3>
                     <h3 className='h3-title-column-inner-heading-main-semester-table'>Title</h3>
                     <h3 className='h3-credits-column-inner-heading-main-semester-table'>Credits</h3>
-                    <h3 className='h3-pre-req-column-inner-heading-main-semester-table'>Pre-Requisite</h3>
+                    {/* <h3 className='h3-pre-req-column-inner-heading-main-semester-table'>Pre-Requisite</h3> */}
                 </div>
 
                 <div className="individual-subject-row-table">
@@ -20,7 +20,7 @@ const Table = (props) => {
                                 <p className='p-code-column-inner-heading-main-semester-table'>{subject.code}</p>
                                 <p className='p-title-column-inner-heading-main-semester-table'>{subject.name}</p>
                                 <p className='p-credits-column-inner-heading-main-semester-table'>{subject.credits[0]} + {subject.credits[1]}</p>
-                                <p className='p-pre-req-column-inner-heading-main-semester-table'>{subject.preRequisite}</p>
+                                {/* <p className='p-pre-req-column-inner-heading-main-semester-table'>{subject.preRequisite}</p> */}
                             </div>
                         ))
                     }
